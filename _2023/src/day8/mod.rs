b946ee5faa04df7d2e90bd11ad5d57a14546a9c9
Christopher J. Adkins <chris@cjadkins.com>
2023-12-08 11:11:24 -0500

    clean up day 8

--- _2023/src/day8/mod.rs
+++ _2023/src/day8/mod.rs
@@ -2,11 +2,12 @@ use std::{collections::HashMap, fs, str::Chars};
 
 use gcd::Gcd;
 
-fn traverse_tree_1(
+fn traverse_tree(
     mut steps_count: u64,
     starting_node: &str,
     directions: &Chars<'_>,
     nodes: &HashMap<&str, (&str, &str)>,
+    ends_with: &str,
 ) -> u64 {
     let mut current_node = starting_node;
 
@@ -17,73 +18,13 @@ fn traverse_tree_1(
             'R' => current_node = nodes.get(&current_node).expect("Node missing").1,
             _ => panic!("Directions should be L or R"),
         }
-        if current_node == "ZZZ" {
+        if current_node.ends_with(ends_with) {
             // we've found the end
             return steps_count;
         }
     }
     // we've gone through the directions once and haven't found the end, we need to go deeper
-    return traverse_tree_1(steps_count, current_node, directions, nodes);
-}
-
-fn traverse_tree_2(
-    mut steps_count: u64,
-    starting_node: &str,
-    directions: &Chars<'_>,
-    nodes: &HashMap<&str, (&str, &str)>,
-) -> u64 {
-    let mut current_node = starting_node;
-
-    for direction in directions.clone() {
-        steps_count += 1;
-        match direction {
-            'L' => current_node = nodes.get(&current_node).expect("Node missing").0,
-            'R' => current_node = nodes.get(&current_node).expect("Node missing").1,
-            _ => panic!("Directions should be L or R"),
-        }
-        if current_node.ends_with('Z') {
-            // we've found the end
-            return steps_count;
-        }
-    }
-    // we've gone through the directions once and haven't found the end, we need to go deeper
-    return traverse_tree_2(steps_count, current_node, directions, nodes);
-}
-
-fn simultaneous_traverse_tree(
-    mut steps_count: u64,
-    starting_nodes: Vec<&str>,
-    directions: &Chars<'_>,
-    nodes: &HashMap<&str, (&str, &str)>,
-) -> u64 {
-    // So this won't work.... we should just use cycles
-    let mut current_nodes: Vec<&str> = starting_nodes.clone();
-
-    for direction in directions.clone() {
-        steps_count += 1;
-
-        for (index, current_node) in current_nodes.clone().iter().enumerate() {
-            match direction {
-                'L' => {
-                    let new_node = nodes.get(current_node).expect("Node missing").0;
-                    current_nodes[index] = new_node;
-                }
-                'R' => {
-                    let new_node = nodes.get(current_node).expect("Node missing").1;
-                    current_nodes[index] = new_node;
-                }
-                _ => panic!("Directions should be L or R"),
-            }
-        }
-
-        if current_nodes.iter().all(|x: &&str| x.ends_with('Z')) {
-            // we've found the end
-            return steps_count;
-        }
-    }
-
-    // we've gone through the directions once and haven't found the end, we need to go deeper
-    return simultaneous_traverse_tree(steps_count, current_nodes, directions, nodes);
+    return traverse_tree(steps_count, current_node, directions, nodes, ends_with);
 }
 
 fn calculate_cycle(
@@ -91,8 +32,8 @@ fn calculate_cycle(
     directions: &Chars<'_>,
     nodes: &HashMap<&str, (&str, &str)>,
 ) -> u64 {
-    let initial_count = traverse_tree_2(0, starting_node, directions, nodes);
-    let cycle_count = traverse_tree_2(initial_count, starting_node, directions, nodes);
+    let initial_count = traverse_tree(0, starting_node, directions, nodes, "Z");
+    let cycle_count = traverse_tree(initial_count, starting_node, directions, nodes, "Z");
     return cycle_count - initial_count;
 }
 
@@ -120,11 +61,11 @@ pub fn main() {
         nodes.insert(node_name.trim(), (left.trim(), right.trim()));
     }
 
-    // let starting_count: i32 = 0;
-    // let starting_node = "AAA";
-    // let steps_count = traverse_tree(starting_count, starting_node, &directions, &nodes);
+    let starting_count: u64 = 0;
+    let starting_node = "AAA";
+    let steps_count = traverse_tree(starting_count, starting_node, &directions, &nodes, "ZZZ");
 
-    // println!("{}", steps_count); // 16043
+    println!("{}", steps_count); // 16043
 
     // part 2
 
@@ -134,10 +75,6 @@ pub fn main() {
         .map(|x| x.as_ref())
         .collect();
 
-    // let starting_count: i32 = 0;
-    // let steps_count =
-    //     simultaneous_traverse_tree(starting_count, starting_nodes, &directions, &nodes);
-
     let cycles: Vec<u64> = starting_nodes
         .iter()
         .map(|x| calculate_cycle(x, &directions, &nodes))
@@ -146,7 +83,6 @@ pub fn main() {
     let mut cycle_count: u64 = 1;
 
     for cycle in cycles {
-        println!("{:?}", cycle);
         cycle_count = lcm(cycle_count, cycle);
     }
 
