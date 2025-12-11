debcaad616beb5c3f26a54506f7b587168ceecae
Christopher J. Adkins <chris@cjadkins.com>
2025-12-10 23:43:11 -0500

    feat: day 10 part 2 attempt

--- _2025/src/day10/main.exs
+++ _2025/src/day10/main.exs
@@ -1,6 +1,6 @@
 day = 10
 dayString = day |> Integer.to_string() |> String.pad_leading(2, "0")
-file = "./_2025/src/day#{dayString}/input.txt"
+file = "./_2025/src/day#{dayString}/example.txt"
 
 IO.puts("Day #{day}")
 
@@ -10,12 +10,14 @@ defmodule AOC do
     |> String.split("\n")
     |> Enum.reject(fn p -> p == "" end)
     |> Enum.map(fn line ->
-      [lights_string | buttons_plus] =
+      [lights_string | buttons_joltage] =
         line
         |> String.split(" ", trim: true)
 
-      [plus | buttons] = buttons_plus |> Enum.reverse()
+      [joltage_string | buttons] = buttons_joltage |> Enum.reverse()
+
       lights_string_length = String.length(lights_string)
+      joltage_string_length = String.length(joltage_string)
 
       lights =
         String.slice(lights_string, 1, lights_string_length - 2)
@@ -32,7 +34,12 @@ defmodule AOC do
           |> Enum.map(&String.to_integer/1)
         end)
 
-      {lights, buttons, plus}
+      joltage =
+        String.slice(joltage_string, 1, joltage_string_length - 2)
+        |> String.split(",", trim: true)
+        |> Enum.map(&String.to_integer/1)
+
+      {lights, buttons, joltage}
     end)
   end
 
@@ -46,6 +53,16 @@ defmodule AOC do
 
   def permutations(list, count) do
     for(elem <- list, rest <- permutations(list -- [elem], count - 1)) do
+      [elem | rest]
+    end
+  end
+
+  def permutations_with_repetition(_, 0) do
+    [[]]
+  end
+
+  def permutations_with_repetition(list, count) do
+    for(elem <- list, rest <- permutations_with_repetition(list, count - 1)) do
       [elem | rest]
     end
   end
@@ -76,12 +93,92 @@ defmodule AOC do
       end
     end)
   end
+
+  def buttons_that_work(buttons, button_index) do
+    buttons
+    |> Enum.reduce([], fn button, acc ->
+      case Enum.find(button, fn c -> c == button_index end) do
+        nil -> acc
+        _ -> [button | acc]
+      end
+    end)
+  end
+
+  def get_joltage_options(buttons, joltage) do
+    for({jotlage_value, index} <- Enum.with_index(joltage)) do
+      options = buttons_that_work(buttons, index)
+      permutations_with_repetition(options, jotlage_value)
+    end
+  end
+
+  def button_to_joltage_form(button, joltage) do
+    joltage
+    |> Enum.with_index()
+    |> Enum.map(fn {_, idx} ->
+      case Enum.any?(button, fn b -> b == idx end) do
+        true -> 1
+        false -> 0
+      end
+    end)
+  end
+
+  def sub(x, y) do
+    Enum.zip(x, y)
+    |> Enum.map(fn {a, b} ->
+      cond do
+        a < b ->
+          :infinity
+
+        true ->
+          a - b
+      end
+    end)
+  end
+
+  def count_buttons(buttons, joltage, cache) do
+    {count, new_cache} =
+      cond do
+        joltage == [0, 0, 0, 0] ->
+          {0, cache}
+
+        Map.has_key?(cache, joltage) ->
+          {Map.get(cache, joltage), cache}
+
+        Enum.any?(joltage, fn j -> j == :infinity end) ->
+          {:infinity, cache}
+
+        true ->
+          buttons
+          |> Enum.scan({0, cache}, fn button, {_, next_cache} ->
+            {count, new_cache} = count_buttons(buttons, sub(joltage, button), next_cache)
+
+            case count do
+              :infinity -> {:infinity, new_cache}
+              _ -> {count + 1, new_cache}
+            end
+          end)
+          |> Enum.min_by(fn {count, _} -> count end)
+      end
+
+    {count, Map.put(new_cache, joltage, count)}
+  end
+
+  def solve_machine_part2({_, buttons, joltage}) do
+    buttons_joltage_form =
+      buttons
+      |> Enum.map(fn button -> button_to_joltage_form(button, joltage) end)
+
+    cache = %{[0, 0, 0, 0] => 0}
+    {count, _} = count_buttons(buttons_joltage_form, joltage, cache)
+    IO.inspect(count)
+    count
+  end
 end
 
 data = AOC.read_input(file)
 
 part1 = data |> Enum.map(&AOC.solve_machine/1) |> Enum.sum()
-part2 = 0
+part2 = data |> Enum.map(&AOC.solve_machine_part2/1) |> Enum.sum()
 
 # 375
 IO.puts("Part 1: #{part1}")
